b1516b0222ef36d11f18a7fb2783afe0b6d86ae3
Erik Beltran Morales <erikb0696@gmail.com>
2025-06-26 16:17:08 -0500

    add ViewMode component

--- src/components/HomePageClient.tsx
+++ src/components/HomePageClient.tsx
@@ -4,16 +4,38 @@ import { useQuery } from "@tanstack/react-query";
 
 import PokemonCard from "./PokemonCard";
 import { getPokemons } from "@/services/api";
+import Image from "next/image";
+import { useState } from "react";
+import { ViewMode } from "./ViewMode";
 
 const HomePageClient = () => {
+  const [viewMode, setViewMode] = useState<"grid" | "table">("grid");
+
   const { data: allPokemon } = useQuery({
     queryKey: ["pokemon-all"],
     queryFn: () => getPokemons(),
     staleTime: 1000 * 60 * 5,
   });
   return (
-    <div className="lg:max-w-4xl lg:mx-auto w-full pt-10 px-6 md:px-10">
-      <h1 className="font-bold text-6xl mb-8 text-center">Pokémon Explorer</h1>
+    <div className="lg:max-w-4xl lg:mx-auto w-full pt-10 px-6 md:px-10 flex flex-col  relative  max-md:overflow-hidden ">
+      <Image
+        src="/pokemon_logo.svg"
+        width={500}
+        height={150}
+        alt="pokemon logo"
+        className="mx-auto mb-4 border  "
+      />
+
+      <Image
+        src="/pokeball-dark.webp"
+        width={400}
+        height={400}
+        alt="pokeo"
+        className="hidden lg:block absolute  opacity-50   -right-1/2 -top-30"
+      />
+
+      <ViewMode value={viewMode} onChange={setViewMode} />
+
       <ul className="grid grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-10">
         {allPokemon?.map((pokemon) => (
           <PokemonCard key={pokemon.name} pokemon={pokemon}></PokemonCard>
---
+++ src/components/ViewMode.tsx
@@ -0,0 +1,33 @@
+import { LayoutGrid, Table } from "lucide-react";
+
+type ViewMode = "grid" | "table";
+
+type ViewModeProps = {
+  value: ViewMode;
+  onChange: (mode: ViewMode) => void;
+};
+
+export const ViewMode = ({ onChange, value }: ViewModeProps) => {
+  return (
+    <div className="flex lg:items-center gap-2 max-md:flex-col max-md:w-full mb-10">
+      <label htmlFor="view-mode" className="text-sm font-medium flex gap-1 ">
+        View
+        {value === "grid" ? (
+          <LayoutGrid className="w-5 h-5 text-gray-600" />
+        ) : (
+          <Table className="w-5 h-5 text-gray-600" />
+        )}
+      </label>
+
+      <select
+        id="view-mode"
+        value={value}
+        onChange={(e) => onChange(e.target.value as ViewMode)}
+        className="border border-gray-400   px-2 py-1 rounded"
+      >
+        <option value="grid">Grid </option>
+        <option value="table">Table </option>
+      </select>
+    </div>
+  );
+};
