ddcd867eba0b14b560c09ef7a428ae99ca9dbe4b
Erik Beltran Morales <erikb0696@gmail.com>
2025-06-26 17:00:42 -0500

    render filteredData on base of pokemon type

--- src/components/HomePageClient.tsx
+++ src/components/HomePageClient.tsx
@@ -1,21 +1,34 @@
 "use client";
 
+import { useMemo, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-
-import { getPokemons } from "@/services/api";
 import Image from "next/image";
-import { useState } from "react";
+
 import { ViewMode } from "./ViewMode";
 import GridView from "./GridView";
+import TypeFilter from "./TypeFilter";
+
+import { getPokemons } from "@/services/api";
 
 const HomePageClient = () => {
   const [viewMode, setViewMode] = useState<"grid" | "table">("grid");
+  const [filterType, setFilterType] = useState("all");
 
   const { data: allPokemon } = useQuery({
     queryKey: ["pokemon-all"],
     queryFn: () => getPokemons(),
     staleTime: 1000 * 60 * 5,
   });
+
+  const filteredData = useMemo(() => {
+    if (!allPokemon) return [];
+    if (filterType === "all") return allPokemon;
+
+    return allPokemon.filter((pokemon) =>
+      pokemon.types.some((t) => t.type.name === filterType)
+    );
+  }, [allPokemon, filterType]);
+
   return (
     <div className="lg:max-w-4xl lg:mx-auto w-full pt-10 px-6 md:px-10 flex flex-col  relative  max-md:overflow-hidden ">
       <Image
@@ -34,9 +47,12 @@ const HomePageClient = () => {
         className="hidden lg:block absolute  opacity-50   -right-1/2 -top-30"
       />
 
-      <ViewMode value={viewMode} onChange={setViewMode} />
+      <div className="flex flex-col lg:flex-row justify-between items-baseline mb-4">
+        <TypeFilter selectedType={filterType} onChange={setFilterType} />
+        <ViewMode value={viewMode} onChange={setViewMode} />
+      </div>
 
-      {viewMode === "grid" ? <GridView data={allPokemon} /> : <table></table>}
+      {viewMode === "grid" ? <GridView data={filteredData} /> : <table></table>}
     </div>
   );
 };
